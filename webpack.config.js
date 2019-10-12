ccdd1df81088d444561702df4d43608ab0d92e2c
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-12 12:58:44 +0800

    feat: Update webpack to use eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,14 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
         test: /\.js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
