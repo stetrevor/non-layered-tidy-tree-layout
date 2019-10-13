f2f636ac8fcfb6f190ab8bbd6b2f7f732402f560
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-13 08:26:17 +0800

    feat: Add Layout class test

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,4 +1,4 @@
-import { BoundingBox } from '../src/helpers'
+import { BoundingBox, Layout } from '../src/helpers'
 
 test('bounding box', () => {
   const bb = new BoundingBox(10, 20)
@@ -14,4 +14,28 @@ test('bounding box', () => {
       y: 120
     })
   )
+})
+
+test('Layout class', () => {
+  const data = {
+    width: 10,
+    height: 10,
+    children: [
+      {width: 10, height: 10, children: [{width: 150, height: 100}]},
+      {width: 10, height: 10, children: []},
+      {width: 10, height: 10, children: []},
+      {width: 10, height: 10, children: []},
+      {width: 10, height: 20, children: []},
+    ]
+  }
+  const bb = new BoundingBox(10, 10)
+  const layout = new Layout(bb)
+  layout.layout(data)
+  expect(data).toEqual(expect.objectContaining({ x: 120, y: 0 }))
+  expect(data.children[0]).toEqual(expect.objectContaining({ x: 75, y: 20 }))
+  expect(data.children[1]).toEqual(expect.objectContaining({ x: 97.5, y: 20 }))
+  expect(data.children[2]).toEqual(expect.objectContaining({ x: 120, y: 20 }))
+  expect(data.children[3]).toEqual(expect.objectContaining({ x: 142.5, y: 20 }))
+  expect(data.children[4]).toEqual(expect.objectContaining({ x: 165, y: 20 }))
+  expect(data.children[0].children[0]).toEqual(expect.objectContaining({ x: 5, y: 40 }))
 })
